d1dfeb8da2efbe7097bc0afc76045559fcc56529
M Cooper Healy <cooper.healy@nabis.com>
2025-12-02 17:20:34 -0600

    Day 1, Part II

--- day_1/day1.jl
+++ day_1/day1.jl
@@ -16,5 +16,4 @@ end
 state = DialState(50, 0)
 println("The dial starts by pointing at ", state.position, ".")
 result = reduce(loudplus, map(Rotation, readlines(ARGS[1])); init=state)
-
 println("\nBecause the dial points at zero a total of ", result.resets, " times during this process, the password in this example is ", result.resets, ".")
--- day_1/src/rotations.jl
+++ day_1/src/rotations.jl
@@ -29,17 +29,22 @@ function Rotation(str::String)
 end
 
 z₀(p) = p == 0 ? 1 : 0
-z₁(p, r) = floor(r.distance / ticks) + (r.distance % ticks) > (r.direction == left ? p : ticks - p)
+function z₁(p₀, p₁, r)
+    Δ = floor(r.distance / ticks)
+    δ = r.distance % ticks
+    δ₊ = p₀ == 0 ? false : δ > (r.direction == left ? p₀ : ticks - p₀)
+    p₊ = p₁ == 0
+    Δ + (δ₊ || p₊ ? 1 : 0)
+end
 
 
-function Base.:+(state::DialState, rot::Rotation)::DialState
-    d = rot.distance % ticks
-    p = rot.direction == left ? state.position - d : state.position + d
-    p₁ = p < 0 ? p + 100 : p
-    p₂ = p₁ % 100
-    # z = z₀(p₂) # Part I
-    z = z₁(state.position, rot)
-    DialState(p₂, state.resets + z)
+function Base.:+(s::DialState, r::Rotation)::DialState
+    d = r.distance % ticks
+    p = r.direction == left ? s.position - d : s.position + d
+    p₁ = (p < 0 ? p + 100 : p) % 100
+    # z = z₀(p₁) # Part I
+    z = z₁(s.position, p₁, r)
+    DialState(p₁, s.resets + z)
 end
 
 Base.show(io::IO, r::Rotation) = print(io, r.direction == left ? "L" : "R", r.distance)
