e12c5e86cc9379104f60f8f33d849d14bf75df91
M Cooper Healy <cooper.healy@nabis.com>
2025-12-03 12:29:12 -0600

    Complete Da3 Part II

--- day_3/day3.jl
+++ day_3/day3.jl
@@ -2,20 +2,27 @@
 
 using Pipe: @pipe
 
-"Find the maximum Joltage for a bank of batteries js"
-function Jₘ(js)
-    d₀ = max(js[1:end-1]...)
-    r = findfirst(j -> j == d₀, js)
-    d₁ = max(js[r+1:end]...)
+function J₁(js, ds, rem)
+    if rem == 0
+        return ds
+    end
 
-    10d₀ + d₁
+    d₀, r = findmax(identity, js[1:end-(rem-1)])
+
+    J₁(js[r+1:end], vcat(ds, d₀), rem - 1)
+end
+
+"Find the maximum Joltage for a bank of batteries js using digits d"
+function Jₘ(js, d)
+    ds = J₁(js, [], d)
+    sum([10^(i - 1) * d for (i, d) in ds |> reverse |> enumerate])
 end
 
 bs2b(bs) = map(b -> parse(Int, b), split(bs, ""))
 
 result = @pipe(
     map(bs2b, readlines(ARGS[1]))
-    |> map(Jₘ, _)
+    |> map(b -> Jₘ(b, 12), _)
     |> sum
 )
 
