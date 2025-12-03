e0d1bcf293ac3f718727a304a10a5c1d212e6c15
M Cooper Healy <cooper.healy@nabis.com>
2025-12-03 13:41:47 -0600

    Clean up day 2

--- day_2/day2.jl
+++ day_2/day2.jl
@@ -1,13 +1,15 @@
 #!/usr/bin/env julia
+
+using Pipe: @pipe
+
 function invalid₂(id::String)::Integer
     l = length(id)
     for d in 1:l-1
-        if l % d != 0 # invalid ids are a repetition of two subsequences, so MUST have an even number of digits
+        if l % d != 0
             continue
         end
 
-        chunks = [id[((i-1)*d+1):(i*d)] for i in 1:Int(l / d)]
-        if allequal(chunks)
+        if allequal([id[((i-1)*d+1):(i*d)] for i in 1:Int(l / d)])
             return parse(Int, id)
         end
     end
@@ -17,7 +19,7 @@ end
 
 function invalid(id::String)::Integer
     l = length(id)
-    if l % 2 != 0 # invalid ids are a repetition of two subsequences, so MUST have an even number of digits
+    if l % 2 != 0
         return 0
     end
     h = Int(l / 2)
@@ -25,13 +27,21 @@ function invalid(id::String)::Integer
     id[1:h] == id[h+1:end] ? parse(Int, id) : 0
 end
 
-rs = split(readline(ARGS[1]), ',')
-Σ = 0
-for r in rs
-    s, e = map(i -> parse(Int, i), split(r, '-'))
-    ι = map(n -> string(n) |> invalid₂, s:e)
-    Σᵣ = reduce(+, ι; init=0)
-    global Σ += Σᵣ
-end
+p(s) = parse(Int, s)
+proc(r) = @pipe(
+    r
+    |> split(_, '-')
+    |> map(p, _)
+    |> range(_...)
+    |> map(invalid₂ ∘ string, _)
+    |> reduce(+, _; init=0)
+)
 
-println("Σ: ", Σ)
+@pipe(
+    (length(ARGS) >= 1 ? ARGS[1] : "input.txt")
+    |> readline
+    |> split(_, ',')
+    |> map(proc, _)
+    |> sum
+    |> println
+)
