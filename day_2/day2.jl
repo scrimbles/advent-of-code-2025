89900e1060dff8624030abe78d3b88c9e549f9d8
M Cooper Healy <cooper.healy@nabis.com>
2025-12-02 18:18:33 -0600

    Part II

--- day_2/day2.jl
+++ day_2/day2.jl
@@ -1,4 +1,19 @@
 #!/usr/bin/env julia
+function invalid₂(id::String)::Integer
+    l = length(id)
+    for d in 1:l-1
+        if l % d != 0 # invalid ids are a repetition of two subsequences, so MUST have an even number of digits
+            continue
+        end
+
+        chunks = [id[((i-1)*d+1):(i*d)] for i in 1:Int(l / d)]
+        if allequal(chunks)
+            return parse(Int, id)
+        end
+    end
+
+    0
+end
 
 function invalid(id::String)::Integer
     l = length(id)
@@ -14,14 +29,9 @@ rs = split(readline(ARGS[1]), ',')
 Σ = 0
 for r in rs
     s, e = map(i -> parse(Int, i), split(r, '-'))
-    ι = map(n -> string(n) |> invalid, s:e)
+    ι = map(n -> string(n) |> invalid₂, s:e)
     Σᵣ = reduce(+, ι; init=0)
     global Σ += Σᵣ
-    # println("")
-    # println(r)
-    # println("ι: ", ι)
-    # println("Σᵣ: ", Σᵣ)
-    # println("Σ: ", Σ)
 end
 
 println("Σ: ", Σ)
