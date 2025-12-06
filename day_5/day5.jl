1837e4e27ec4f422566fd5969fe4b338c8be0bbd
M Cooper Healy <m.cooper.healy@gmail.com>
2025-12-05 18:02:08 -0600

    [WIP] Part II still wrong

--- day_5/day5.jl
+++ day_5/day5.jl
@@ -2,7 +2,6 @@
 
 using Pipe: @pipe
 
-isfresh(ing, lst) = any(rec -> ing >= rec[1] && ing <= rec[2], lst)
 
 function parsefile(filename)
 
@@ -27,12 +26,48 @@ function parsefile(filename)
     ranges, ingredients
 end
 
+isfresh(ing, lst) = any(rec -> ing >= rec[1] && ing <= rec[2], lst)
 freshingredients(ranges, ingredients) = map(ing -> isfresh(ing, ranges), ingredients) |> sum
-allfresh(ranges) = 0
+
+function combine(R)
+    R₁ = []
+    a₁, b₁ = 0, 0
+    for r ∈ R
+        a₂, b₂ = r
+        if a₂ > b₁
+            # no overlap, store current range to R₁
+            if a₁ != b₁ # ignore the starting 0,0
+                push!(R₁, Pair(a₁, b₁))
+            end
+            a₁, b₁ = a₂, b₂
+        else
+            # overlap, replace our b with the maximum between b₁ and b₂
+            b₁ = max(b₁, b₂)
+        end
+    end
+    if a₁ != R₁[end][1]
+        push!(R₁, Pair(a₁, b₁))
+    end
+
+    R₁
+end
+
+allfresh(ranges) = @pipe(
+    ranges
+    |> sort(_; by=r -> r[1])
+    |> combine
+    |> map(p -> (p[2] - p[1]) + 1, _)
+    |> sum
+)
 
 
-filename = length(ARGS) > 1 ? ARGS[1] : "input.txt"
-ranges, ingredients = parsefile(filename)
 
-println("Available fresh ingredients: ", freshingredients(ranges, ingredients))
-println("Number of fresh ingredients possible: ", allfresh(filename))
+function main(filename)
+    ranges, ingredients = parsefile(filename)
+
+    println("Available fresh ingredients: ", freshingredients(ranges, ingredients))
+    println("Number of fresh ingredients possible: ", allfresh(ranges))
+end
+
+filename = length(ARGS) >= 1 ? ARGS[1] : "input.txt"
+main(filename)
