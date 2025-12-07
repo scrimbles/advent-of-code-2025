e2a21c2a78ca5ec77f3acc7e97b74ac6f95ba818
M Cooper Healy <m.cooper.healy@gmail.com>
2025-12-07 00:18:37 -0600

    Fix broken input

--- day_6/day6.jl
+++ day_6/day6.jl
@@ -2,52 +2,47 @@
 
 using Pipe: @pipe
 
-ϕ(str) = str == "+" ? Base.:+ : Base.:*
-solve(prob) = reduce(ϕ(prob[end]), map(n -> parse(Int, n), prob[1:end-1]))
+ϕ(s) = s == "+" || s == '+' ? Base.:+ : Base.:*
 
-function chunk(vec)
-    chunked = []
-    current = []
-    for value in vec
-        if ismissing(value)
-            push!(chunked, current)
-            current = []
-        else
-            append!(current, value)
-        end
-    end
+solve(prob) = @pipe(
+    prob
+    |> filter(s -> all(isnumeric, s), _)
+    |> map(n -> parse(Int, n), _)
+    |> reduce(ϕ(prob[end]), _)
+)
 
-    push!(chunked, current)
-    chunked
-end
+solve₂(prob) = @pipe(
+    prob
+    |> map(p -> filter(isnumeric, p) |> collect |> String, _)
+    |> map(n -> parse(Int, n), _)
+    |> reduce(ϕ(prob[1][end]), _)
+)
+
+splitᵥ(λ, a::Vector) = @pipe(
+    [firstindex(a) - 1; findall(λ, a); lastindex(a) + 1]
+    |> tuple(@view(_[1:end-1]), @view(_[2:end]))
+    |> zip(_...)
+    |> map(x -> view(a, x[1]+1:x[2]-1), _)
+)
 
 part1(filename) = @pipe(
     readlines(filename)
     |> map(split, _)
     |> zip(_...)
+    |> map(collect, _)
     |> map(solve, _)
     |> sum
 )
 
-function part2(filename)
-    lines = readlines(filename)
-    nums = @pipe(
-        lines[1:end-1]
-        |> map(collect, _)
-        |> zip(_...)
-        |> map(s -> string(s...), _)
-        |> map(s -> all(isspace, s) ? missing : parse(Int, s), _)
-        |> chunk
-    )
-
-    ops = lines[end] |> split |> s -> map(ϕ, s)
-
-    problems = [[nums[i]..., ops[i]] for i in eachindex(ops)]
-
-    map(p -> reduce(p[end], p[1:end-1]), problems) |> sum
-end
-
-
+part2(filename) = @pipe(
+    readlines(filename)
+    |> map(collect, _)
+    |> zip(_...)
+    |> collect
+    |> splitᵥ(p -> all(isspace, p), _)
+    |> map(solve₂, _)
+    |> sum
+)
 
 filename = length(ARGS) >= 1 ? ARGS[1] : "input.txt"
 println("Part I: ", part1(filename))
