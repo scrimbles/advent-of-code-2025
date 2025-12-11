eec510c9419cd43fe3767a28a35012e58dbdfa16
M Cooper Healy <m.cooper.healy@gmail.com>
2025-12-11 01:02:46 -0600

    Complete day 11

--- day_11/day11.jl
+++ day_11/day11.jl
@@ -2,40 +2,50 @@
 
 using Pipe: @pipe
 using DataStructures
+using Memoization: @memoize
 
 parseinput(filename) = @pipe(
     filename
     |> readlines(_)
     |> map(l -> split(l, ": "), _)
-    |> map(p -> (p[1], split(p[2])), _)
+    |> map(p -> (Symbol(p[1]), map(Symbol, split(p[2]))), _)
     |> foldl((d, (source, dests)) -> begin
             d[source] = dests
             d
         end, _; init=DefaultDict([]))
 )
 
-function numpaths(graph, start, finish)
+@memoize function numpaths(start, finish; avoid=nothing)
+    if start == avoid
+        return 0
+    end
+
+    if start == finish
+        return 1
+    end
+
     paths = 0
-    generation = graph[start]
-
-    while length(generation) > 0
-        outs = findall(s -> s == finish, generation)
-        paths += length(outs)
-        deleteat!(generation, outs)
-
-        generation′ = []
-        for k in generation
-            append!(generation′, graph[k])
-        end
-        generation = generation′
+    for child in GRAPH[start]
+        paths += numpaths(child, finish; avoid)
     end
 
     paths
 end
 
-part1(graph) = numpaths(graph, "you", "out")
+function part2()
+    srv2fft = numpaths(:svr, :fft; avoid=:dac)
+    fft2dac = numpaths(:fft, :dac)
+    dac2out = numpaths(:dac, :out; avoid=:fft)
+
+    srv2dac = numpaths(:svr, :dac; avoid=:fft)
+    dac2fft = numpaths(:dac, :fft)
+    fft2out = numpaths(:fft, :out; avoid=:dac)
+
+    srv2fft * fft2dac * dac2out + srv2dac * dac2fft * fft2out
+end
 
 filename = length(ARGS) >= 1 ? ARGS[1] : "input.txt"
-graph = parseinput(filename)
-println(part1(graph))
-println(part2(graph))
+
+GRAPH = parseinput(filename)
+println("Part I: ", numpaths(:you, :out))
+println("Part II: ", part2())
